6683b4a0c27eabb146fde7d1b94214621244fec2
Tealeaf <ShintaoMonk@gmail.com>
2019-11-11 16:17:52 -0500

    Added getItemVariables

--- src/resource/Compendium.java
+++ src/resource/Compendium.java
@@ -3,6 +3,8 @@ package resource;
 import classes.*;
 
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.*;
 
 public class Compendium {
@@ -16,8 +18,7 @@ public class Compendium {
 		String displayHTML = util.getContents(SITE + "/w/" + name.replace(' ', '_'));
 		String editContents = getEditContents(util.getContents(SITE + "/index.php?title=" + name.replace(' ', '_') + "&action=edit"));
 		
-		
-		
+		List<String[]> vars = getItemVariables(editContents);
 		
 		return ret;
 	}
@@ -26,8 +27,33 @@ public class Compendium {
 		final String START = "name=\"wpTextbox1\">", END = "</textarea>";
 		return HTML.substring(HTML.indexOf(START) + START.length(), HTML.indexOf(END));
 	}
+	
+	
 	private static String getItemType(String editContents) {
 		String ret = editContents.substring(11);
 		return ret.substring(0,ret.indexOf('|'));
+	}
+	
+	private static List<String[]> getItemVariables(String editContents) {
+		List<String[]> ret = new ArrayList<String[]>();
+		
+		String temp = "";
+		int index = 0;
+		for(char c : editContents.toCharArray()) {
+			if(c == '|' && index == 2) {
+				if(temp.contains("=")) {
+					String name = temp.substring(0,temp.indexOf('='));
+					String data = temp.substring(temp.indexOf('=') + 1);
+					if(temp.contains(" =")) name = name.substring(0,name.length() - 1);
+					if(temp.contains("= ")) data = data.substring(1);
+					System.out.println(name + "|" + data);
+					ret.add(new String[] {name, data});
+				}
+				temp = "";
+			} else temp+=c;
+			if(c == '{') index++;
+			if(c == '}') index--;
+		}
+		return ret;
 	}
 }
