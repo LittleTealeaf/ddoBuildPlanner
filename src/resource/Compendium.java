d96db4638477fa29b40c9230e80bcf126c3fdf4b
Tealeaf <ShintaoMonk@gmail.com>
2019-11-11 15:37:29 -0500

    Create Compendium.java

---
+++ src/resource/Compendium.java
@@ -0,0 +1,21 @@
+package resource;
+
+import classes.*;
+
+import java.net.*;
+import java.io.*;
+
+public class Compendium {
+	
+	private static final String siteBase = "https://ddocompendium.com/w/";
+	
+	public Item getItem(String name) {
+		Item ret = new Item();
+		
+		
+		
+		
+		return ret;
+	}
+	
+}
