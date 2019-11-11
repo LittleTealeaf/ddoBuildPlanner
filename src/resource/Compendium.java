abcf186aab17fce2719f1ae150bd9a24d2b7e22c
Tealeaf <ShintaoMonk@gmail.com>
2019-11-11 15:57:00 -0500

    Added getItemType

--- src/resource/Compendium.java
+++ src/resource/Compendium.java
@@ -17,11 +17,17 @@ public class Compendium {
 		String editContents = getEditContents(util.getContents(SITE + "/index.php?title=" + name.replace(' ', '_') + "&action=edit"));
 		
 		
+		
+		
 		return ret;
 	}
 	
 	private static String getEditContents(String HTML) {
 		final String START = "name=\"wpTextbox1\">", END = "</textarea>";
 		return HTML.substring(HTML.indexOf(START) + START.length(), HTML.indexOf(END));
+	}
+	private static String getItemType(String editContents) {
+		String ret = editContents.substring(11);
+		return ret.substring(0,ret.indexOf('|'));
 	}
 }
