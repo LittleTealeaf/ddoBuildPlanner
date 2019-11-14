a3de1da58155a9cd8c3ea9a5cccb8931a24eb136
Tealeaf <35083315+LittleTealeaf@users.noreply.github.com>
2019-11-14 10:16:04 -0500

    Update Gearsets.java

--- src/fxTabs/Gearsets.java
+++ src/fxTabs/Gearsets.java
@@ -4,23 +4,33 @@ import java.util.Optional;
 
 import classes.*;
 import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
 public class Gearsets {
+	
+	public static Tab tab;
+	
 	public static Tab getTab() {
-		Tab ret = new Tab("Gearsets");
+		
+		final Insets DEFAULTINSETS = new Insets(10,10,10,10);
+		
+		tab = new Tab("Gearsets");
 		BorderPane content = new BorderPane();
 		
 		ChoiceBox<Build.Gear> gearChoice = new ChoiceBox<Build.Gear>(FXCollections.observableArrayList(Build.gearSets));
+		gearChoice.setOnAction(action -> {
+			
+		});
 		
 		HBox hHeader = new HBox(gearChoice);
 		hHeader.setSpacing(10);
 		content.setTop(hHeader);
 		
-		ret.setContent(content);		
-		return ret;
+		tab.setContent(content);		
+		return tab;
 	}
 }
