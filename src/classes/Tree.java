42efea43ba241ce23cb5edbb09326b2f0272e238
Tealeaf <35083315+LittleTealeaf@users.noreply.github.com>
2019-11-18 13:43:33 -0500

    Added requirements / enhancement reqs

--- src/classes/Tree.java
+++ src/classes/Tree.java
@@ -6,10 +6,26 @@ import javafx.scene.image.Image;
 
 public class Tree {
   
+  public int width;
+  public int height;
+  
+  public List<Enhanement> Enhancements;
+  
+  //Returns a borderpane with a sort system
   
   public static class Enhancement {
     public String name;
     public String description;
+    
+    //x pos, y pos, or core position
+    public int x;
+    public int y;
+    public int c;
+    
+    //Prerequisites
+    public int reqPoints;
+    public List<Enhancement> reqEnhancements;
+    //public List<Requirement> preReqs;
     
     public Image icon;
     
@@ -20,6 +36,9 @@ public class Tree {
       name = "";
       description = "";
       icon = null;
+      reqPoints = 0;
+      reqEnhancements = new ArrayList<Enhancement>();
+      //preReqs = new ArrayList<Requirement>();
     }
     
   }
