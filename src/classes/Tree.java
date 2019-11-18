90425ee408466f17c235c1ee04e37884f44e4f1d
Tealeaf <35083315+LittleTealeaf@users.noreply.github.com>
2019-11-18 13:28:26 -0500

    Updated Tree.java

--- src/classes/Tree.java
+++ src/classes/Tree.java
@@ -1,5 +1,24 @@
 package classes;
 
-public class Tree {
+import javafx.scene.image.Image;
 
+public class Tree {
+  
+  
+  public static class Enhancement {
+    public String name;
+    public String description;
+    
+    public Image icon;
+    
+    public List<Attribute> attributes;
+    
+    public Enhancement() {
+      attributes = new ArrayList<Attribute>();
+      name = "";
+      description = "";
+      icon = null;
+    }
+    
+  }
 }
