1b1ed6540b22b83a9024eec8b6629d3bc12f8820
Tealeaf <ShintaoMonk@gmail.com>
2019-11-27 15:50:42 -0500

    Organized Imports

--- src/application/fxMain.java
+++ src/application/fxMain.java
@@ -1,14 +1,13 @@
 package application;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class fxMain extends Application {
--- src/fxTabs/Gearsets.java
+++ src/fxTabs/Gearsets.java
@@ -1,26 +1,7 @@
 package fxTabs;
 
-import java.util.Optional;
-
-import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar.ButtonData;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 
 public class Gearsets {
 	
