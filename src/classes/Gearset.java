875433760b7402de51d2442726e9937a2acfa2e4
Tealeaf <ShintaoMonk@gmail.com>
2019-11-27 18:25:17 -0500

    Removed Un-Needed Code

--- src/classes/Gearset.java
+++ src/classes/Gearset.java
@@ -5,12 +5,9 @@ public class Gearset {
 	public String name;
 	public Item goggles, helmet, necklace, trinket, armor, cloak, bracers, belt, ring1, ring2, boots, gloves;
 
-	public Gearset() {
-		for(Item a : getItems()) a = null;
-	}
+	public Gearset() {}
 
 	public Gearset(String name) {
-		for(Item a : getItems()) a = null;
 		name = "";
 	}
 
