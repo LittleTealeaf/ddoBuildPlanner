433d88e6f7ca113a8d725ca493368fc5a08493d8
Tealeaf <ShintaoMonk@gmail.com>
2019-11-27 16:50:23 -0500

    Create Attribute.java

---
+++ src/classes/Attribute.java
@@ -0,0 +1,19 @@
+package classes;
+
+import java.util.List;
+
+public class Attribute {
+
+	public String name; //ID
+	public String type;
+	public int value;
+	
+	public List<String> checks; //List of checks required before ability is given
+	//TODO Requirement Class
+	
+	public Attribute() {
+		name = "";
+		type = "";
+		value = 0;
+	}
+}
