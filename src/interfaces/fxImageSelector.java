40b17c341bf194d0d2e119ce7469ae02ae4e7d94
Tealeaf <35083315+LittleTealeaf@users.noreply.github.com>
2019-12-05 10:10:57 -0500

    Organized Import

--- src/interfaces/fxImageSelector.java
+++ src/interfaces/fxImageSelector.java
@@ -1,16 +1,13 @@
 package interfaces;
 
 import java.io.File;
-import java.io.InputStream;
-
-import javax.imageio.stream.FileImageInputStream;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.TextField;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
