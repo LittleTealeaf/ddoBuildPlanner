c195c5429d4c29a5ad88f65e31e4bbd8ea73e275
Tealeaf <ShintaoMonk@gmail.com>
2019-12-08 13:38:41 -0500

    Create fxEditEnchantment.java

---
+++ src/interfaces/fxEditEnchantment.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import classes.Enchantment;
+import javafx.stage.Stage;
+
+public class fxEditEnchantment {
+
+	public static Stage stage;
+	
+	public static void open(Enchantment ench) {
+		
+	}
+}
