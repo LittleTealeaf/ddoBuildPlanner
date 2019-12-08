af1d7d14b1219599236b883152e7a96a75399e53
Tealeaf <ShintaoMonk@gmail.com>
2019-12-08 16:49:00 -0500

    Update fxInventory.java

--- src/interfaces/fxInventory.java
+++ src/interfaces/fxInventory.java
@@ -14,6 +14,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class fxInventory {
