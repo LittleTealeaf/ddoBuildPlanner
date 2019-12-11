77784f937da5cd134c87a9f0697bd17e1fe07d07
Tea <shintaomonk@gmail.com>
2019-12-11 12:00:52 -0500

    QUICK

--- src/interfaces/fxEditEnchantment.java
+++ src/interfaces/fxEditEnchantment.java
@@ -3,14 +3,18 @@ package interfaces;
 import classes.Enchantment;
 import classes.Enchantments;
 import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -31,20 +35,26 @@ public class fxEditEnchantment {
 		stage = new Stage();
 		stage.setTitle((enchantment == null) ? "Create Enchantment" : "Edit Enchantment " + ench.getName());
 		
+		
+		VBox content = new VBox();
+		
 		Text lName = new Text("Name");
 		
 		TextField name = new TextField();
 		name.setText(ench.getName());
 		name.textProperty().addListener((e,o,n) -> ench.setName(n));
 		
-		HBox hb = new HBox(lName,name);
+		HBox hName = new HBox(lName,name);
+		hName.setSpacing(10);
+		content.getChildren().add(content);
+		
+		content.getChildren().add(variablePane());
+		
+		
 		
 		Button bSave = new Button("Save");
 		bSave.setOnAction(e -> save());
-		
-		BorderPane content = new BorderPane();
-		content.setTop(hb);
-		content.setBottom(bSave);
+
 		
 		stage.setScene(new Scene(content));
 		stage.show();
@@ -52,5 +62,16 @@ public class fxEditEnchantment {
 	
 	private static void save() {
 		Enchantments.updateEnchantment(ench);
+	}
+	
+	private static TitledPane variablePane() {
+		TitledPane r= new TitledPane();
+		
+		Text text = new Text();
+		
+		r.setContent(text);
+		r.setPadding(new Insets(10));
+		
+		return r;
 	}
 }
