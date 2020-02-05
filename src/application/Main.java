4a5bdd43eadc45ecfd13a42bd764b0d749fb078d
BobdaFett <lvas463@gmail.com>
2020-02-05 13:06:15 -0500

    fuck you

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,8 +19,6 @@ public class Main {
 	public static void main(String[] args) {
 		loadedBuild = new Build();
 
-		// THIS IS MA EDIT
-
 		Debug.setCrashReporting();
 		system.loadData();
 		Settings.loadSettings();
